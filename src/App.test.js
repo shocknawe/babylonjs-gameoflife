8c2be74df5a577a991760e79e9d1e7006d44eb5e
Jairus Ser Aragon <jairus.aragon@zuhlke.com>
2021-04-23 23:26:54 +0800

    configure tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import {shallow} from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const app = wrapper.find('[data-test="app"]');
+  expect(app.exists()).toBe(true);
 });
